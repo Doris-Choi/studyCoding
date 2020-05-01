9adbb3333caf98e56fbb6a8fc44cd1a28e6c7048
doris <dychoi9298@gmail.com>
2020-05-02 01:02:50 +0900

    draft: LinkedList

--- dataStructure/LinkedList.py
+++ dataStructure/LinkedList.py
@@ -2,20 +2,31 @@
 # LinkedList.py
 
 class Node:
-    def __init__(self, data, next=None):
+    def __init__(self, data):
         self.data = data
-        self.next = next
+        self.next = None
 
 class LinkedList:
-    def __init__(self, data):
-        self.head = Node(data)
-        self.idx = 0
+    def __init__(self):
+        self.head = None
+        self.idx = -1
 
-    # 맨 뒤에 데이터 추가
-    def add(self, data):
+    # 맨 앞에 데이터 추가
+    def insertFront(self, data):
         if self.head == None:
             self.head = Node(data)
-            self.idx = 0
+            self.idx += 1
+        else:
+            node = self.head
+            self.head = Node(data)
+            self.head.next = node
+            self.idx += 1
+
+    # 맨 뒤에 데이터 추가
+    def insertEnd(self, data):
+        if self.head == None:
+            self.head = Node(data)
+            self.idx += 1
         else:
             cur = self.head
             while cur.next:
@@ -23,52 +34,114 @@ class LinkedList:
             cur.next = Node(data)
             self.idx += 1
 
-    # 맨 앞에 데이터 추가
-    def addFront(self, data):
-        if self.head == None:
+    # 특정 위치 데이터 추가
+    def insertAt(self, data, index=0):
+        if index > self.idx + 1:
+            print('INDEX:: out of range')
+        elif index == 0:
+            cur = self.head
             self.head = Node(data)
-            self.idx = 0
-        else:
-            node = self.head
-            self.head = Node(data)
-            self.head.next = node
+            self.head.next = cur
             self.idx += 1
+        elif index == self.idx + 1:
+            cur = self.head
+            while cur.next:
+                cur = cur.next
+            cur.next = Node(data)
+            self.idx += 1
+        else:
+            cur = self.head
+            for _ in range(index-1):
+                cur = cur.next
+            node = cur.next
+            cur.next = Node(data)
+            cur.next.next = node
+            self.idx += 1
+
 
     # 맨 앞에 데이터 삭제
     def deleteFront(self):
         if self.head == None:
-            print('overflow 발생')
+            print('empty List')
         else:
-            self.head = self.head.next
+            node = self.head
+            self.head = node.next
+            del node
             self.idx -= 1
 
-    # 특정 위치에 데이터 추가
-    # def insertAt(self, data, idx):
-
-    # 특정 위치에 데이터 삭제
-    # def deleteAt(self, idx):
-
-    # 특정 값을 갖는 데이터의 첫 번째 위치 반환
-    def search(self, data):
-        ith = 0
+    # 맨 뒤에 데이터 삭제
+    def deleteEnd(self):
         if self.head == None:
-            print("해당 리스트가 비어있습니다.")
-            return False
+            print('empty List')
+        else:
+            cur = self.head
+            for _ in range(self.idx-1):
+                cur = cur.next
+            node = cur.next
+            del node
+            cur.next = None
+            self.idx -= 1
 
-        if self.head.data == data:
-            return ith
+    # 특정 위치 데이터 삭제
+    def deleteAt(self, index=0):
+        if self.head == None:
+            print('empty List')
+        elif index > self.idx:
+            print('INDEX:: out of range')
+        elif index == 0:
+            node = self.head
+            self.head = node.next
+            del node
+            self.idx -= 1
+        elif index == self.idx:
+            cur = self.head
+            for _ in range(self.idx-1):
+                cur = cur.next
+            node = cur.next
+            del node
+            cur.next = None
+            self.idx -= 1
+        else:
+            cur = self.head
+            for _ in range(index-1):
+                cur = cur.next
+            node = cur.next
+            cur.next = node.next
+            del node
+            self.idx -= 1
 
-        cur = self.head
-        while cur.next:
-            if cur.data == data:
-                return ith
-            else:
-                ith += 1
+
+    # 특정 값을 갖는 데이터의 첫 번째 인덱스 반환
+    def searchIndex(self, data):
+        if self.head == None:
+            print("empty List")
+        else:
+            cur = self.head
+            ith = 0
+            while ith <= self.idx:
+                if cur.data == data:
+                    return ith
+                else:
+                    cur = cur.next
+                    ith += 1
+
+            # 동일한 값이 없는 경우
+            return -1
+
+    # 특정 인덱스의 값을 반환
+    def searchData(self, index):
+        if self.head == None:
+            print('empty List')
+        else:
+            cur = self.head
+            for _ in range(index):
+                cur = cur.next
+            return cur.data
 
     # linked list 출력
     def showAll(self):
         if self.head == None:
-            print('해당 리스트가 비어있습니다.')
+            print('empty List')
         else:
             cur = self.head
             while cur.next:
@@ -76,28 +149,43 @@ class LinkedList:
                 cur = cur.next
             print(cur.data)
 
-if __name__ == "__main__":
-    ll = LinkedList(4)
-    # test add
-    ll.add(3)
-    ll.add(2)
-    ll.showAll()
-    # test addFront
-    ll.addFront(1)
-    ll.addFront(0)
-    ll.showAll()
-    # test deleteFront
-    ll.deleteFront()
-    ll.showAll()
-    # test deleteFront with empty LinkedList
-    ll.deleteFront()
-    ll.deleteFront()
-    ll.deleteFront()
-    ll.deleteFront()
-    ll.deleteFront()
-    # test showAll with empty LinkedList
-    ll.showAll()
 
-    ll.add(4)
-    ll.showAll()
+if __name__ == "__main__":
+    ll = LinkedList()
+
+    # test insert
+    ll.insertEnd(3)
+    ll.insertFront(2)
     print(ll.idx)
+    ll.showAll()
+    ll.insertAt(4, index=4)
+    ll.insertAt(1)
+    ll.insertAt(5, index=3)
+    ll.insertAt(4, index=3)
+    ll.showAll()
+    print()
+
+    # test delete
+    ll.deleteFront()
+    ll.showAll()
+    ll.deleteEnd()
+    ll.showAll()
+    ll.deleteAt()
+    ll.showAll()
+    ll.deleteAt(index=4)
+    ll.insertFront(1)
+    ll.deleteAt(index=1)
+    ll.showAll()
+    print()
+
+    # test search
+    ll.insertEnd(4)
+    ll.insertEnd(3)
+    ll.insertEnd(4)
+    ll.insertEnd(2)
+    ll.insertEnd(7)
+    ll.showAll()
+    print(ll.searchIndex(4))
+    ll.deleteAt(ll.searchIndex(4))
+    ll.showAll()
+    print(ll.searchData(ll.searchIndex(4)))
