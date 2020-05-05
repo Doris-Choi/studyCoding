299b686eb71c84f1b9f009a2b2316e0a25b1b66f
doris <dychoi9298@gmail.com>
2020-05-05 21:41:13 +0900

    NEW FILE:: Stack.py

---
+++ dataStructure/Stack.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+# stack.py
+
+class Node():
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class Stack():
+    def __init__(self):
+        self.top = None
+
+    def push(self, data):
+        if self.top == None:
+            self.top = Node(data)
+        else:
+            cur = self.top
+            self.top = Node(data)
+            self.top.next = cur
+
+    def pop(self):
+        if self.top == None:
+            print("empty Stack")
+        else:
+            cur = self.top
+            self.top = cur.next
+            print(cur.data)
+            return cur.data
+
+    def show(self):
+        if self.top == None:
+            print("empty Stack")
+        else:
+            cur = self.top
+            print("top ->", end = ' ')
+            while cur.next:
+                print(cur.data, end = ' ')
+                cur = cur.next
+            print(cur.data)
+
+
+if __name__ == "__main__":
+    s = Stack()
+    s.push(1)
+    s.push(2)
+    s.push(3)
+    s.push(4)
+    s.show()
+    s.pop()
+    s.pop()
+    s.pop()
+    s.pop()
+    s.pop()
+    s.show()
